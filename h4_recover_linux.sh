d8750ce7a35c266f208ca70aae707efa889da14d
evilwombat <evilwombat@server.fake>
2016-01-06 13:50:47 -0800

    gboot: Add prebuilt Mac OS version
    Add a prebuilt version of gpboot_mac, along with recovery
    wrapper scripts for Mac OS and Linux.
    
    Signed-off-by: evilwombat <evilwombat@server.fake>

---
+++ h4_recover_linux.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+echo "If you are missing libusb, you can install it with:"
+echo "  sudo apt-get install libusb-1.0.0-dev"
+sudo ./gpboot --h4-recovery
+
---
+++ h4_recover_mac.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+sudo DYLD_LIBRARY_PATH=. ./gpboot_mac --h4-recovery
+
